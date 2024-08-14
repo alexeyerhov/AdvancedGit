03aed63ad685b1892360f9ab5c5ce01adae6daf6
Alexey <>
2024-08-14 14:29:27 +0300

    added comment

---
+++ AdvancedGit/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AdvancedGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // added comment
+        }
+    }
+}
